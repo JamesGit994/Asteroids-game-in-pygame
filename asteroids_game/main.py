18e6d8ccf675763e9b4ae6deda11b99fac103969
JamesGit994 <jamesjamneck@gmail.com>
2025-08-11 11:35:19 +0200

    drawing player trainge

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,15 +1,25 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        delte_time = fps.tick(60)
+        dt = delte_time / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
