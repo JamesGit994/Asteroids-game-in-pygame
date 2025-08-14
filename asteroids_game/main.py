9414f91b49d8e48a8ad4e8e4fa15333b2cf2586c
JamesGit994 <jamesjamneck@gmail.com>
2025-08-14 14:25:22 +0200

    made groups for player class

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -8,7 +8,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,8 +23,9 @@ def main():
         delte_time = fps.tick(60)
         dt = delte_time / 1000
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
 
